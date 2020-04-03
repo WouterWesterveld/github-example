106d1de0f6c8c91d8013b9600ea3ab1fa7c830b8
WouterWesterveld <57808203+WouterWesterveld@users.noreply.github.com>
2020-04-03 14:34:57 +0200

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,6 +1,8 @@
+This notebook will be mainly used for the capstone project
+Import pandas as pd
+Import Numpy as np
+print "Hello Capstone Project Course!"
 
-print 'Helloe Github'
-print 'Hello'
-i
-print "Hello"
+
+
 
